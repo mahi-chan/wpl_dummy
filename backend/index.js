f7cdd879ecd2097d50a637374e32b086af33697a
ashiquzzaman-rahad <ashiquerahad.007@gmail.com>
2024-09-07 14:59:03 +0600

    update backend function

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express()
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"love",
+    password:"210201",
     database:"test"
 })
 
@@ -21,6 +21,20 @@ app.get("/books", (req,res)=>{
         return res.json(data)
     })
 })
+
+app.put('/', (req,res)=>{
+    const q = "update `books` set `title`= (?), `desc`=(?), `cover`=(?) where `id`= (?) ";
+    const id = req.body.id;
+    const title = req.body.title;
+    const desc = req.body.desc;
+    const cover = req.body.cover;
+
+    db.query(q, [id, title, desc, cover], (err, data)=>{
+        if (err) return res.json("Error!");
+        return res.json(data);
+    })
+})
+
 
 app.listen(8800, ()=>{
     console.log("Connected to backend!1")
