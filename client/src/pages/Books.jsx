44bc53f147ad0bece98462c369ea3a014d5caaa7
Sun-Desh <sandeshsapkota54@gmail.com>
2024-09-08 19:47:27 +0600

    Added delete and upload function and some css work

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -14,14 +14,35 @@ const Books = () => {
             }catch(err){
                 console.log(err)
             }
+<<<<<<< HEAD
+        };
+        fetchAllBooks();
+=======
         }
         fetchAllBooks()
+>>>>>>> c3050cc99ea790761fb957f1a77c04da1350422e
     },[])
 
     return (
         <div>
             <h1>Lama Book Shop</h1>
             <div className="books">
+<<<<<<< HEAD
+                {books.map((book) => (
+                    <div className="book" key={book}>
+                        {book.cover && <img src={book.cover} alt="" />}
+                        <h2>{book.title}</h2>
+                        <p>{book.desc}</p>
+                        <span>{book.price}</span>
+                        <button className="delete">Delete</button>
+                        <button className="update">Update</button>
+                        </div>
+                ))}
+            </div>
+            <button>
+                <Link to="/add">Add new book</Link>
+            </button>
+=======
                 {books.map(book=>(
                     <div className="book" key={book.id}>
                         <h2>{book.title}</h2>
@@ -30,7 +51,8 @@ const Books = () => {
                     </div>
                 ))}
             </div>
+>>>>>>> c3050cc99ea790761fb957f1a77c04da1350422e
         </div>
     )
-}
-export default Books
+};
+export default Books;
